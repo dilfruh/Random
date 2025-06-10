91cda2ff7d58ce04e525420784cc421fde76c64c
dilfruh <dillon.fruhwirth@outlook.com>
2025-06-10 11:16:33 -0500

    added 2k and madden

--- app/src/main/java/dil/random/MainActivity.java
+++ app/src/main/java/dil/random/MainActivity.java
@@ -557,7 +557,7 @@ public class MainActivity extends AppCompatActivity {
     public void gameClicked(View view) {
         stopMediaPlayers();
 
-        String[] games = {"Sacrifice Darren to the Sun God", "Rocket League", "Fall Guys", "Fortune Street", "Smash", "Mario Kart", "Mario Party Superstars", "Mario Party 4", "Mario Golf", "Mario Tennis", "Original Strikers", "Strikers: Battle League", "Mario Superstar Baseball"};
+        String[] games = {"Sacrifice Darren to the Sun God", "Rocket League", "Fall Guys", "Fortune Street", "Smash", "Mario Kart", "Mario Party Superstars", "Mario Party 4", "Mario Golf", "Mario Tennis", "Original Strikers", "Strikers: Battle League", "Mario Superstar Baseball", "2K", "Madden"};
         int gameIndex = getRandomIndex(games);
         String gameName = games[gameIndex];
 
